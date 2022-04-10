28f406cf9091b55c0dc210c639f0427b656e661c
choi woo seok <cwooseok60@gmail.com>
2022-04-10 15:40:04 +0900

    HttpResposne message nullable 하게 수정

--- src/main/kotlin/com/shallwecode/common/http/response/HttpResponse.kt
+++ src/main/kotlin/com/shallwecode/common/http/response/HttpResponse.kt
@@ -1,10 +1,8 @@
 package com.shallwecode.common.http.response
 
-import org.springframework.http.HttpStatus
-
 class HttpResponse<T>(
     val status: Int,
-    val message: String,
+    val message: String? = null,
     val body: T? = null
 ) {
 }
