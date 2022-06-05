dbb1770707c3fd404d40d39bd431e32608d33a41
choi woo seok <cwooseok60@gmail.com>
2022-06-06 04:38:39 +0900

    ProjectCreateRequestUnitTest.kt 생성

---
+++ src/test/kotlin/com/shallwecode/project/controller/ProjectCreateRequestUnitTest.kt
@@ -0,0 +1,14 @@
+package com.shallwecode.project.controller
+
+import org.junit.jupiter.api.Test
+
+class ProjectCreateRequestUnitTest {
+    @Test
+    fun toEntityTest() {
+        // given
+
+        // when
+
+        // then
+    }
+}
